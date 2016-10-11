b3be7c62daace488d09c4d8947c86920d440ff41
joel schaubert <joel.schaubert@gmail.com>
2016-10-10 21:09:58 -0500

    more prep for allremote

--- custard/db/schema.rb
+++ custard/db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160821203138) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "facilities", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
