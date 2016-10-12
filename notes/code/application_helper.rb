252dbe8ff51cc510cbe3f70e13b120288be51061
joel schaubert <joel.schaubert@gmail.com>
2016-10-11 21:10:06 -0500

    final walkthrough adjustments

--- notes/code/application_helper.rb
+++ notes/code/application_helper.rb
@@ -1,4 +1,4 @@
-
+module ApplicationHelper
   def csv_upload link
     html = "<p>Import from CSV file</p>\n".html_safe
     html << form_tag( link, multipart: true )
@@ -6,4 +6,5 @@
       html << '  <input type="submit" name="commit" value="Import" />'.html_safe
     html << "</form><br/>".html_safe
   end
+end
 
